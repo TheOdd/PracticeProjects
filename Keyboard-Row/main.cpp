af8497b72f42ddf7619680d2a1cee7f85dc12511
TheOdd <owenhines07@gmail.com>
2017-02-14 16:05:33 -0600

    Fixed up and added TODO messages.

--- Keyboard-Row/main.cpp
+++ Keyboard-Row/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-#include <string.h>
+#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
+vector<string> findWords(vector<string>& words) 
+{
+    // TODO: Research more on unordered_set, vector, usage of <>, & char arrays.
+    
+    unordered_set<char> row1 {'q', 'w', 'e', 'r', 't', 'y','u', 'i', 'o', 'p'};
+    unordered_set<char> row2 {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'}; 
+    unordered_set<char> row3 { 'z', 'x', 'c', 'v', 'b' ,'n', 'm'};
+    vector<unordered_set<char>> rows {row1, row2, row3};
+    
+    // TODO: Remove this return statement as soon as able to, as it only makes the compiler not give a warning
+    vector<basic_string<char>> placeholder;
+    return placeholder;
+}
+
 int main()
 {
-    char array1[] = "`1234567890-=~!@#$%^&*()_+";
-    char array2[] = "qwertyuiop[]\\QWERTYUIOP{}|";
-    char array3[] = "asdfghjkl;'ASDFGHJKL:\"";
-    char array4[] = "zxcvbnm,./ZXCVBNM<>?";
-    /*
-    Use these values, but there is probably a better way to store and manage them.
-    */
+    
 }
