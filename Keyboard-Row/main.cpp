cabd3e543a4bcd22db30f9bd33d2a485f5bc83a9
TheOdd <owenhines07@gmail.com>
2017-02-14 15:24:04 -0600

    Added base code for keyboard row project.

---
+++ Keyboard-Row/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char array1[] = "`1234567890-=~!@#$%^&*()_+";
+    char array2[] = "qwertyuiop[]\\QWERTYUIOP{}|";
+    char array3[] = "asdfghjkl;'ASDFGHJKL:\"";
+    char array4[] = "zxcvbnm,./ZXCVBNM<>?";
+    /*
+    Use these values, but there is probably a better way to store and manage them.
+    */
+}
