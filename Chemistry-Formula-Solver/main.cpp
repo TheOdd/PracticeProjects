e363ae3937d3b7bc6c08690c41dd590fd44df08f
TheOdd <owenhines07@gmail.com>
2017-04-19 12:20:07 -0500

    More progress on flexible loop-checking. TODO: Error-checking.

--- Chemistry-Formula-Solver/main.cpp
+++ Chemistry-Formula-Solver/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 std::string x;
-bool invalid = false;
+bool invalidMenu = false;
 int target;
 std::string variableStr;
 std::vector<double> options = {0, 0, 0, 0};
@@ -22,14 +22,14 @@ int selectLaw() {
     "3.) Gay-Lussac's Law - P1/T1 = P2/T2" << std::endl <<
     "4.) Avogadro's Principle - V1/n1 = V2/n2" << std::endl <<
     "5.) Ideal Gas Law - PV = nRT" << std::endl;
-    if(invalid) {
+    if(invalidMenu) {
         std::cout << "Please enter a valid option!" << std::endl << std::endl;
     }
     std::cout << std::fixed << std::showpoint;
     std::cout << std::setprecision(4);
     std::cin >> x;
     system("clear");
-    invalid = false;
+    invalidMenu = false;
     if (x == "1") {
         std::cout << boyOperation() << std::endl;
         std::cin.get();
@@ -46,60 +46,48 @@ int selectLaw() {
         std::cout << idealOperation() << std::endl;
         std::cin.get();
     } else {
-        invalid = true;
+        invalidMenu = true;
         selectLaw();
     }
     return 0;
 }
 
 double boyOperation() {
-    std::string variableStr;
-    double P1;
-    double P2;
-    double V1;
-    double V2;
+    std::vector<std::string> stringOptions = {"P1", "P2", "V1", "V2"};
+    int P1 = 0, P2 = 1, V1 = 2, V2 = 3;
     std::cout << "P1 x V1 = P2 x V2" << std::endl << "Which variable are you solving for?" << std::endl;
     std::cin >> variableStr;
     system("clear");
-    if (variableStr == "P1") {
-        std::cout << "What is the value of P2?" << std::endl;
-        std::cin >> P2;
-        std::cout << "What is the value of V1?" << std::endl;
-        std::cin >> V1;
-        std::cout << "What is the value of V2?" << std::endl;
-        std::cin >> V2;
-        P1 = (P2 * V2)/V1;
-        return P1;
-    } else if (variableStr == "P2") {
-        std::cout << "What is the value of P1?" << std::endl;
-        std::cin >> P1;
-        std::cout << "What is the value of V1?" << std::endl;
-        std::cin >> V1;
-        std::cout << "What is the value of V2?" << std::endl;
-        std::cin >> V2;
-        P2 = (P1 * V1)/V2;
-        return P2;
-    } else if (variableStr == "V1") {
-        std::cout << "What is the value of P1?" << std::endl;
-        std::cin >> P1;
-        std::cout << "What is the value of P2?" << std::endl;
-        std::cin >> P2;
-        std::cout << "What is the value of V2?" << std::endl;
-        std::cin >> V2;
-        V1 = (P2 * V2)/P1;
-        return V1;
-    } else if (variableStr == "V2") {
-        std::cout << "What is the value of P1?" << std::endl;
-        std::cin >> P1;
-        std::cout << "What is the value of P2?" << std::endl;
-        std::cin >> P2;
-        std::cout << "What is the value of V1?" << std::endl;
-        std::cin >> V1;
-        V2 = (P1 * V1)/P2;
-        return V2;
-    } else {
-        std::cout << "Please enter a valid option!" << std::endl;
-        boyOperation();
+    bool invalid = true;
+    while (invalid) {
+        for (int i = 0; i < 4; i++) {
+            if (variableStr == stringOptions[i])
+            invalid = false;
+        }
+        if (invalid) {
+            system("clear");
+            std::cout << "Please enter a valid option!" << std::endl;
+            std::cin >> variableStr;
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        if (variableStr == stringOptions[i]) {
+            target = i;
+            continue;
+        }
+        else {
+            std::cout << "What is the value of " << stringOptions[i] << "?" << std::endl;
+            std::cin >> options[i];
+        }
+    }
+    if (target == P1) {
+        return (options[P2]*options[V2])/options[V1];
+    } else if (target == P2) {
+        return (options[P1]*options[V1])/options[V2];
+    } else if (target == V1) {
+        return (options[P2]*options[V2])/options[P1];
+    } else if (target == V2) {
+        return (options[P1]*options[V1])/options[P2];
     }
     return 0;
 }
@@ -148,105 +136,87 @@ double charOperation() {
 }
 
 double lusOperation() {
-    std::string variableStr;
-    double P1;
-    double P2;
-    double T1;
-    double T2;
+    std::vector<std::string> stringOptions = {"P1", "P2", "T1", "T2"};
+    int P1 = 0, P2 = 1, T1 = 2, T2 = 3;
     std::cout << "P1/T1 = P2/T2" << std::endl << "Which variable are you solving for?" << std::endl;
     std::cin >> variableStr;
     system("clear");
-    if (variableStr == "P1") {
-        std::cout << "What is the value of P2?" << std::endl;
-        std::cin >> P2;
-        std::cout << "What is the value of T1 in Kelvin?" << std::endl;
-        std::cin >> T1;
-        std::cout << "What is the value of T2 in Kelvin?" << std::endl;
-        std::cin >> T2;
-        P1 = (P2/T2)*T1;
-        return P1;
-    } else if (variableStr == "P2") {
-        std::cout << "What is the value of P1?" << std::endl;
-        std::cin >> P1;
-        std::cout << "What is the value of T1 in Kelvin?" << std::endl;
-        std::cin >> T1;
-        std::cout << "What is the value of T2 in Kelvin?" << std::endl;
-        std::cin >> T2;
-        P2 = (P1/T1)*T2;
-        return P2;
-    } else if (variableStr == "T1") {
-        std::cout << "What is the value of P1?" << std::endl;
-        std::cin >> P1;
-        std::cout << "What is the value of P2?" << std::endl;
-        std::cin >> P2;
-        std::cout << "What is the value of T2 in Kelvin?" << std::endl;
-        std::cin >> T2;
-        T1 = P1/(P2/T2);
-        return T1;
-    } else if (variableStr == "T2") {
-        std::cout << "What is the value of P1?" << std::endl;
-        std::cin >> P1;
-        std::cout << "What is the value of P2" << std::endl;
-        std::cin >> P2;
-        std::cout << "What is the value of T1 in Kelvin?" << std::endl;
-        std::cin >> T1;
-        T2 = P2/(P1/T1);
-        return T2;
-    } else {
-        std::cout << "Please enter a valid option!" << std::endl;
-        lusOperation();
+    bool invalid = true;
+    while (invalid) {
+        for (int i = 0; i < 4; i++) {
+            if (variableStr == stringOptions[i])
+                invalid = false;
+        }
+        if (invalid) {
+            system("clear");
+            std::cout << "Please enter a valid option!" << std::endl;
+            std::cin >> variableStr;
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        if (variableStr == stringOptions[i]) {
+            target = i;
+            continue;
+        }
+        else {
+            std::cout << "What is the value of " << stringOptions[i] << "?" << std::endl;
+            std::cin >> options[i];
+        }
+    }
+    if (target == P1) {
+        return (options[P2]/options[T2])*T1;
+    }
+    else if (target == P2) {
+        return (options[P1]/options[T1])*T2;
+    }
+    else if (target == T1) {
+        return options[P1]/(options[P2]/options[T2]);
+    }
+    else if (target == T2) {
+        return options[P2]/(options[P1]/options[T1]);
     }
     return 0;
 }
 
 double avogOperation() {
-    std::string variableStr;
-    double V1;
-    double V2;
-    double n1;
-    double n2;
+    std::vector<std::string> stringOptions = {"V1", "V2", "n1", "n2"};
+    int V1 = 0, V2 = 1, n1 = 2, n2 = 3;
     std::cout << "V1/n1 = V2/n2" << std::endl << "Which variable are you solving for?" << std::endl;
     std::cin >> variableStr;
     system("clear");
-    if (variableStr == "V1") {
-        std::cout << "What is the value of V2?" << std::endl;
-        std::cin >> V2;
-        std::cout << "What is the value of n1?" << std::endl;
-        std::cin >> n1;
-        std::cout << "What is the value of n2?" << std::endl;
-        std::cin >> n2;
-        V1 = (V2/n2)*n1;
-        return V1;
-    } else if (variableStr == "V2") {
-        std::cout << "What is the value of V1?" << std::endl;
-        std::cin >> V1;
-        std::cout << "What is the value of n1?" << std::endl;
-        std::cin >> n1;
-        std::cout << "What is the value of n2?" << std::endl;
-        std::cin >> n2;
-        V2 = (V1/n1)*n2;
-        return V2;
-    } else if (variableStr == "n1") {
-        std::cout << "What is the value of V1?" << std::endl;
-        std::cin >> V1;
-        std::cout << "What is the value of V2?" << std::endl;
-        std::cin >> V2;
-        std::cout << "What is the value of n2?" << std::endl;
-        std::cin >> n2;
-        n1 = V1/(V2/n2);
-        return n1;
-    } else if (variableStr == "n2") {
-        std::cout << "What is the value of V1?" << std::endl;
-        std::cin >> V1;
-        std::cout << "What is the value of V2?" << std::endl;
-        std::cin >> V2;
-        std::cout << "What is the value of n1?" << std::endl;
-        std::cin >> n1;
-        n2 = V2/(V1/n1);
-        return n2;
-    } else {
-        std::cout << "Please enter a valid option!" << std::endl;
-        avogOperation();
+    bool invalid = true;
+    while (invalid) {
+        for (int i = 0; i < 4; i++) {
+            if (variableStr == stringOptions[i])
+                invalid = false;
+        }
+        if (invalid) {
+            system("clear");
+            std::cout << "Please enter a valid option!" << std::endl;
+            std::cin >> variableStr;
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        if (variableStr == stringOptions[i]) {
+            target = i;
+            continue;
+        }
+        else {
+            std::cout << "What is the value of " << stringOptions[i] << "?" << std::endl;
+            std::cin >> options[i];
+        }
+    }
+    if (target == V1) {
+        return (options[V2]/options[n2])*options[n1];
+    }
+    else if (target == V2) {
+        return (options[V1]/options[n1])*options[n2];
+    }
+    else if (target == n1) {
+        return options[V1]/(options[V2]/options[n2]);
+    }
+    else if (target == n2) {
+        return options[V2]/(options[V1]/options[n1]);
     }
     return 0;
 }
